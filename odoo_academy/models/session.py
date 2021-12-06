70fe86bd5a1532c9988ca474dd0f14b84fc7a8dc
cyro-odoo <94933410+cyro-odoo@users.noreply.github.com>
2021-12-06 11:30:31 -0800

    Update session.py

--- odoo_academy/models/session.py
+++ odoo_academy/models/session.py
@@ -6,4 +6,11 @@ class Session(models.Model):
   _name = 'academy.session'
   _description = 'Session Info'
   
+  course_id = fields.Many2one(comodel_name='academy.course',
+                              string='Course',
+                              ondelete='cascade'
+                              required=True)
+  
+  name = fields.Char(string='Title', related='course_id.name')
+  
   
